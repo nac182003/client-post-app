cf044ca8ba63fdbeaa781731631b46a530fd28ea
nac182003 <nac.182003@gmail.com>
2025-05-31 02:34:22 -0500

    client-post-app

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,11 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
     {
+        path: 'auth',
+        loadChildren:()=> import('./auth/auth.routes'),
+    },
+    {
         path: '',
         loadChildren:()=> import('./front-page/front.routes'),
-    }
+    },
 ];
